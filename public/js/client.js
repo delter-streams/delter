0b68229d4e75c6a17c444fc1d125501833e78b9f
takuti <k.takuti@gmail.com>
2016-02-21 04:23:51 +0900

    style

--- public/js/client.js
+++ public/js/client.js
@@ -2,18 +2,22 @@ var socket = io();
 
 socket.on('update', function(item){
   var p = document.createElement('p');
+  p.className = 'item';
 
   var img = document.createElement('img');
   img.src = item.icon;
   img.alt = 'icon';
-  p.appendChild(img);
 
-  p.appendChild(document.createElement('br'));
+  var id = document.createElement('a');
+  id.href = 'http://twitter.com/' + item.name;
+  id.appendChild(img);
 
-  var a = document.createElement('a');
-  a.href = item.url;
-  a.innerText = item.title;
-  p.appendChild(a);
+  p.appendChild(id);
+
+  var link = document.createElement('a');
+  link.href = item.url;
+  link.innerText = ' ' + item.title;
+  p.appendChild(link);
 
   var div = document.getElementsByClassName('list')[0];
   div.insertBefore(p, div.firstChild);
